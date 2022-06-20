f7d016ccecefe02aa91f51dae610722b87c6b956
Vladimir Mokhov <mohov_vova@mail.u>
2022-06-20 21:53:52 +0300

    First commit

---
+++ HW_6/src/main/java/pro/sky/hw_6/EmployeeService.java
@@ -0,0 +1,47 @@
+package pro.sky.hw_6;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class EmployeeService {
+
+    private static final int LIMIT = 10;
+
+    private final List<Employee> employees = new ArrayList<>();
+
+    public Employee add(String name, String surname) {
+        Employee employee = new Employee(name, surname);
+        if (employees.contains(employee)) {
+            throw new EmployeeAlreadyAddedException();
+        }
+        if (employees.size() < LIMIT) {
+            employees.add(employee);
+            return employee;
+        }
+        throw new EmployeeStoragelsFullException();
+    }
+
+    public Employee remove(String name, String surname) {
+        Employee employee = new Employee(name, surname);
+        if (!employees.contains(employee)) {
+            throw new EmployeeNotFoundException();
+        }
+        employees.remove(employee);
+        return employee;
+    }
+
+    public Employee find(String name, String surname) {
+        Employee employee = new Employee(name, surname);
+        if (!employees.contains(employee)) {
+            throw new EmployeeNotFoundException();
+        }
+        return employee;
+    }
+
+    public List<Employee> getAll() {
+        return new ArrayList<>(employees);
+    }
+}
---
+++ HW_6/src/test/java/pro/sky/hw_6/Hw6ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.hw_6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
